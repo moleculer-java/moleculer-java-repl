0e017ee0334f263411c77dae7222c084d446b353
Berkes Andras <andras.berkes@mail.com>
2018-12-01 18:07:41 +0100

    Ping command

---
+++ src/main/java/services/moleculer/repl/commands/Ping.java
@@ -0,0 +1,82 @@
+/**
+ * THIS SOFTWARE IS LICENSED UNDER MIT LICENSE.<br>
+ * <br>
+ * Copyright 2018 Andras Berkes [andras.berkes@programmer.net]<br>
+ * Based on Moleculer Framework for NodeJS [https://moleculer.services].
+ * <br><br>
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:<br>
+ * <br>
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.<br>
+ * <br>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package services.moleculer.repl.commands;
+
+import static services.moleculer.repl.ColorWriter.CYAN;
+import static services.moleculer.repl.ColorWriter.GREEN;
+import static services.moleculer.repl.ColorWriter.YELLOW;
+import static services.moleculer.util.CommonUtils.formatNamoSec;
+
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+import io.datatree.Tree;
+import services.moleculer.ServiceBroker;
+import services.moleculer.repl.Command;
+import services.moleculer.service.Name;
+
+/**
+ * Query another Moleculer Node on the network to determine whether there is a
+ * connection to it.
+ */
+@Name("ping")
+public class Ping extends Command {
+
+	@Override
+	public String getDescription() {
+		return "Sends a PING packet to the specified node";
+	}
+
+	@Override
+	public String getUsage() {
+		return "ping <nodeID>";
+	}
+
+	@Override
+	public int getNumberOfRequiredParameters() {
+		return 1;
+	}
+
+	@Override
+	public void onCommand(ServiceBroker broker, PrintWriter out, String[] parameters) throws Exception {
+		String nodeID = parameters[0];
+		out.println(YELLOW + ">> Sending PING to '" + nodeID + "' node...");
+		long start = System.nanoTime();
+		Tree rsp = broker.ping(nodeID).waitFor(10, TimeUnit.SECONDS);
+		long duration = System.nanoTime() - start;
+		out.println();
+		out.println(CYAN + "Execution time: " + formatNamoSec(duration));
+		out.println();
+		out.println(GREEN + "Response:");
+		out.println();
+		if (rsp == null) {
+			out.println("'null' response");
+		} else {
+			out.println(rsp.toString("colorized-json", true, true));
+		}
+	}
+
+}
